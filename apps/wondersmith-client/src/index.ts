b37df88f1f177f037696d9d47013cd3a3547accb
murt <murt@unpossible.io>
2022-05-13 17:16:10 +1000

    Launching a blank client and turbo will now launch the client specifically from the root project

--- apps/wondersmith-client/src/index.ts
+++ apps/wondersmith-client/src/index.ts
@@ -1,1 +1,6 @@
-console.log("wondersmith-client started");
+import { app, BrowserWindow } from "electron";
+
+(async () => {
+    await app.whenReady();
+    new BrowserWindow({ width: 800, height: 600 });
+})();
