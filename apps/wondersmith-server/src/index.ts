ce243ae4c7845a979c2652b0f507fda70e496bbe
murt <murt@unpossible.io>
2022-05-19 16:00:02 +1000

    Safe shutdown of the API server now in place

--- apps/wondersmith-server/src/index.ts
+++ apps/wondersmith-server/src/index.ts
@@ -8,7 +8,22 @@ Client.init("postgresql://wonder:smith@localhost:5432/wondersmith");
 (async () => await migrate(Client.get()))();
 
 // Start a new API server
-new WondersmithAPIServer({ port: 4242 }).start();
+const wsAPIServer = new WondersmithAPIServer({ port: 4242 });
+
+// Multiple ways of ending a process so we want to take the first one that works and only use it
+// then - the process may hang for a bit while all connections are cleanly shut down.
+let killCount = 0;
+const killer = async () => {
+    if (killCount++ === 0) {
+        console.log("Stopping...");
+        await wsAPIServer.stop();
+        console.log("Stopped");
+    }
+};
+["SIGINT", "SIGQUIT", "SIGTERM"].forEach(sig => process.on(sig, killer));
+
+// Start the API server
+wsAPIServer.start();
 
 /*
 const io = new Server(server);
