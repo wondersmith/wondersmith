df8c3fa476a6759764e84ffb44c852a36ca7f052
murt <murt@unpossible.io>
2022-05-29 20:44:45 +1000

    Adding some shared linting config

--- apps/wondersmith-server/src/config/index.ts
+++ apps/wondersmith-server/src/config/index.ts
@@ -4,6 +4,9 @@ import { WondersmithAPIServerConfig } from "wondersmith-api-server/dist/config.j
 
 import argv from "./args.js";
 
+/**
+* Generates the config for the server from a config file and / or command line arguments.
+*/
 export default function getConfig(args: NodeJS.Process["argv"]): WondersmithAPIServerConfig {
     const parsedArgs = argv(args);
     const configFile = cosmiconfigSync(parsedArgs.config ?? "config");
